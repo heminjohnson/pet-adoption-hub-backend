91fa21a1dbd467fe64c20b66d14807033b208ad4
Hemin Johnson <hemin.johnson@diconium.com>
2023-03-24 22:34:01 +0100

    feat: add basic services for pets

--- src/pets/pets.controller.ts
+++ src/pets/pets.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('pets')
-export class PetsController {}
+export class PetsController {
+  @Get()
+  findAll() {
+    return 'This action returns all pets';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return `This action returns a #${id} pet`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+    //return 'This action adds a new pet';
+  }
+
+  @Patch(':id')
+  update(@Param('id') id, @Body() body) {
+    return `This action updates a #${id} pet`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id) {
+    return `This action removes a #${id} pet`;
+  }
+}
