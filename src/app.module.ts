db04811a35791755095bf2653a27a70e6d945c33
Hemin Johnson <hemin.johnson@diconium.com>
2023-03-24 22:12:03 +0100

    feat: add pets controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PetsController } from './pets/pets.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [AppController, PetsController],
   providers: [AppService],
 })
 export class AppModule {}
